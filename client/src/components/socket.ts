e7a10d2f63173d6b53153d3ad221effc130ff871
AvtoTheThird <avtonariashvili@gmail.com>
2025-01-26 16:27:07 +0400

    fix

--- client/src/components/socket.ts
+++ client/src/components/socket.ts
@@ -2,7 +2,11 @@
 import { io } from "socket.io-client";
 
 const socket = io(
-  "https://654d-2a0b-6204-4e3-1500-719c-d6be-8dad-6aa1.ngrok-free.app"
+  "https://654d-2a0b-6204-4e3-1500-719c-d6be-8dad-6aa1.ngrok-free.app",
+  {
+    transports: ["websocket"], // Force WebSocket transport
+    secure: true, // Use HTTPS
+  }
 );
 console.log("socket", socket);
 

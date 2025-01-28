d8621b8b5a7a373368bb099474bdd8856d3152f9
AvtoTheThird <avtonariashvili@gmail.com>
2025-01-28 17:31:06 +0400

    modified:   client/src/components/socket.ts

--- client/src/components/socket.ts
+++ client/src/components/socket.ts
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 const socket = io(
-  "https://654d-2a0b-6204-4e3-1500-719c-d6be-8dad-6aa1.ngrok-free.app",
+  "https://ddeb-2a0b-6204-4e3-1500-9d0c-2361-d688-1b33.ngrok-free.app ",
   {
     transports: ["websocket"], // Force WebSocket transport
     secure: true, // Use HTTPS

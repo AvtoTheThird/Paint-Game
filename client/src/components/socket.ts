37b43f787617197c8ff45aa946ad5e8c5938ae9f
AvtoTheThird <avtonariashvili@gmail.com>
2025-02-12 22:15:55 +0400

    test

--- client/src/components/socket.ts
+++ client/src/components/socket.ts
@@ -1,16 +1,13 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const socket = io(
-  "https://7029-2a0b-6204-4e3-1500-a5c7-9caf-1302-7fa3.ngrok-free.app",
-  {
-    transports: ["websocket"], // Force WebSocket transport
-    secure: true, // Use HTTPS
-    reconnection: true,
-    reconnectionAttempts: 5, // Number of retry attempts
-    reconnectionDelay: 1000, // Delay in ms between retries
-  }
-);
+const socket = io("http://195.238.122.103:3000", {
+  transports: ["websocket"], // Force WebSocket transport
+  secure: true, // Use HTTPS
+  reconnection: true,
+  reconnectionAttempts: 5, // Number of retry attempts
+  reconnectionDelay: 1000, // Delay in ms between retries
+});
 console.log("socket", socket);
 
 // https://paint-game.onrender.com

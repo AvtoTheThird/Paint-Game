a741a192f856b164b85ea49e567f64507c93ad25
AvtoTheThird <avtonariashvili@gmail.com>
2025-02-12 23:33:24 +0400

    AAAA

--- client/src/components/socket.ts
+++ client/src/components/socket.ts
@@ -1,7 +1,7 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("http://xelovniki.zapto.org", {
+const socket = io("https://xelovniki.zapto.org", {
   transports: ["websocket"], // Force WebSocket transport
   secure: true, // Use HTTPS
   reconnection: true,

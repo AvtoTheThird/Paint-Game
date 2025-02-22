cd1a4159f972219b67d20bb2337db49ad80b20b9
AvtoTheThird <avtonariashvili@gmail.com>
2025-02-22 14:01:57 +0400

    modified:   client/src/components/socket.ts

--- client/src/components/socket.ts
+++ client/src/components/socket.ts
@@ -1,7 +1,7 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://xelovniki.zapto.org", {
   transports: ["websocket"], // Force WebSocket transport
   secure: true, // Use HTTPS
   reconnection: true,

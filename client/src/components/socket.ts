25d6b6907bd2846810d0303c428d8851d1cd8a8d
AvtoTheThird <avtonariashvili@gmail.com>
2025-04-26 20:10:13 +0400

    aaaaaaaaaaaaaaa

--- client/src/components/socket.ts
+++ client/src/components/socket.ts
@@ -1,7 +1,7 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("https://api.khelovniki.com", {
+const socket = io("http://51.21.219.17:3000", {
   transports: ["websocket", "polling"], // Force WebSocket transport
   secure: true, // Use HTTPS
   reconnection: true,

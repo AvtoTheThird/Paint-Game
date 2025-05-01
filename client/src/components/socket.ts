758786a9b05a72737125aef47b66ed1bc4ff6635
AvtoTheThird <avtonariashvili@gmail.com>
2025-05-01 17:12:00 +0400

    AAAAAAAAAAA

--- client/src/components/socket.ts
+++ client/src/components/socket.ts
@@ -1,7 +1,7 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("localhost:3000/", {
+const socket = io("https://api.khelovniki.com/", {
   transports: ["websocket", "polling"],
   reconnection: true,
   reconnectionAttempts: 5,
